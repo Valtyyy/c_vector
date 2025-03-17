9a90e6a45643922fef0c9cf2cae095a3ffd03eb6
valtyyy <valentind2006@outlook.fr>
2025-03-17 17:07:41 +0100

    refactor (utils) : split_array_v rewritten + moved prototype to c_vector_utils.h header

--- src/utils/split_array_v.c
+++ src/utils/split_array_v.c
@@ -11,49 +11,57 @@
 #include <stddef.h>
 #include <stdio.h>
 
-static int add_string(vector_t *src, char *string, const char separator)
+static int add_string(vector_t *src, vector_t *str)
 {
-    char *cpy = my_strdup(string);
-    char sep[2] = {0};
+    char *string = NULL;
 
-    sep[0] = separator;
-    if (!cpy)
+    vector_push(str, &(char){'\0'});
+    string = my_strdup(str->data);
+    if (!string)
         return FAIL;
-    strstrip(cpy, sep);
-    if (*cpy == 0)
-        return SUCCESS;
-    vector_push(src, &cpy);
+    vector_push(src, &string);
+    vector_clear(str);
     return SUCCESS;
 }
 
 static int add_sub(vector_t *src, vector_t *sub)
 {
-    vector_t *dup = vector_duplicate(sub, NULL);
+    vector_t *sub_dup = vector_duplicate(sub, NULL);
 
-    if (!dup)
-        return SUCCESS;
-    vector_push(src, &dup);
+    if (!sub_dup)
+        return FAIL;
+    vector_push(src, &sub_dup);
     vector_clear(sub);
     return SUCCESS;
 }
 
-vector_t *split_array_v(vector_t *src, const char separator)
+static void deal_matrix(char *matrix, vector_t *sub, vector_t *splitted,
+    const char *separators)
+{
+    V_AUTOFREE vector_t *str_buff = init_vector(1, sizeof(char));
+
+    for (ssize_t i = 0; matrix[i] != 0; i++) {
+        if (!my_str_contain(separators, matrix[i])) {
+            vector_push(str_buff, &matrix[i]);
+            continue;
+        }
+        add_string(sub, str_buff);
+        add_sub(splitted, sub);
+    }
+    if (str_buff->size > 0)
+        add_string(sub, str_buff);
+}
+
+vector_t *split_array_v(vector_t *src, const char *separators)
 {
     vector_t *splitted = init_vector(1, sizeof(vector_t *));
-    char **strings = VECTOR_DATA(src, char *);
-    V_AUTOFREE vector_t *sub = init_vector(1, sizeof(char *));
+    V_AUTOFREE vector_t *sub = init_vector(1, sizeof(vector_t *));
+    char **matrix = VECTOR_DATA(src, char *);
 
     if (!splitted || !src)
         return NULL;
-    for (ssize_t i = 0; i < src->size; i++) {
-        if (!strings[i]) {
-            add_sub(splitted, sub);
-            continue;
-        }
-        add_string(sub, strings[i], separator);
-        if (my_str_contain(strings[i], separator) == 1)
-            add_sub(splitted, sub);
-    }
+    for (ssize_t i = 0; i < src->size; i++)
+        deal_matrix(matrix[i], sub, splitted, separators);
     if (sub->size > 0)
         add_sub(splitted, sub);
     return splitted;
